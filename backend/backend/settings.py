7b60d1c52d099fc47503ecc37c5601bba058c210
GhaziGithub <gqj3893@autuni.ac.nz>
2022-11-15 23:18:08 +1300

    switch to prod mode

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,6 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 DEV_MODE = False
+
 SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
