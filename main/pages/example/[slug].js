8f5ab694f32a4b8a1d70f8d7692c598e35ef1040
GhaziGithub <gqj3893@autuni.ac.nz>
2023-02-06 11:10:47 +1300

    adding a new simple component

---
+++ main/pages/example/[slug].js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+
+export async function getServerSideProps({params}){
+    try{
+    const res = await axios.get(`${HOST}articles/${params.slug}/`)
+    const data = await res.data
+    return {
+      props: {data:data}, // will be passed to the page component as props
+    }
+  }
+  catch(e){
+    console.log("here")
+    console.log(e)
+  
+    return {
+      props:{}
+    }
+  }
+  }
+
+
+export default function Component({data}){
+    return(
+        <div>
+            {data.title}
+            {data.description}
+        </div>
+    )
+}
+
